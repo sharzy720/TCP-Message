681b4b342a6c9a3bb82f1b528e206e8cd456cc42
Johnathyn Strong <johnathynstrong@gmail.com>
2021-10-06 17:49:26 -0400

    Finished writing and commenting Server.java

--- message/src/Server.java
+++ message/src/Server.java
@@ -5,27 +5,37 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/**
+ * Creates a simple TCP server that receives a message from a client and sends it back
+ * @version 10-6-21
+ */
 public class Server {
+    /**
+     * The main method of the server does all the needed operations of the server.
+     * @param args No commandline arguments are expected
+     */
     public static void main(String[] args) {
-        int port = 1234;
-        boolean autoFlush = true;
+        int port = 1234; // Port number to be used when receiving and sending messages
+        boolean autoFlush = true; // To turn autoFlush on or off
 
         try {
             ServerSocket serverSocket = new ServerSocket(port);
 
-            while (serverSocket.isClosed() != true) {
-                Socket socket = new Socket();
-                socket = serverSocket.accept();
+            /* Keeps the server open till manually closed*/
+            while (!serverSocket.isClosed()) {
+                Socket socket = serverSocket.accept();
 
-                InputStreamReader inputStreamReader =
-                        new InputStreamReader(socket.getInputStream());
+                InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
                 BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
                 PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), autoFlush);
 
+                /* Reads the string that is sent by the client.
+                * bufferedReader returns a String but the value can be parsed to any data type
+                * needed*/
                 String input = bufferedReader.readLine();
-                int output = Integer.parseInt(input);
 
-                printWriter.println(output);
+                /* Sending the string back to the Client*/
+                printWriter.println(input);
 
                 printWriter.close();
                 bufferedReader.close();
@@ -35,7 +45,7 @@ public class Server {
 
             serverSocket.close();
         } catch (IOException ioException) {
-            System.out.println(ioException);
+            System.out.println(ioException.getMessage());
         }
     }
 }
